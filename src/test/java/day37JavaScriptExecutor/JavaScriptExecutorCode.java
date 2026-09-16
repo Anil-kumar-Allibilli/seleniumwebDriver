3f0f6f2d9e84dd4535eeeaa9e0c7f23d283ff697
Anil kumar Allibilli <allibillianil@gmail.com>
2026-09-16 11:35:54 +0530

    JavaScriptExecutor code
    JavaScriptExecutor

---
+++ src/test/java/day37JavaScriptExecutor/JavaScriptExecutorCode.java
@@ -0,0 +1,30 @@
+package day37JavaScriptExecutor;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptExecutorCode {
+
+	
+	// when we perform click() action or try to interact with webelements internally JavaScript statements and this statements communicate with webelements, if sometimes when click method is not able to execute javaScript then it will throw "Element intercepted exception", so we use directly JavaScript instead of using click
+	public static void main(String[] args) 
+	
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/#");
+		driver.manage().window().maximize();
+		
+		//normal way is .sendkeys("Anil kumar")
+		WebElement inputbox = driver.findElement(By.xpath("//input[@id='name']"));
+		
+		//using JavaScriptExecutors
+		// for ChromeDriver driver = new ChromeDriver(); we can write JavascriptExecutor js = driver; becasue we are string chromreDriver(child) into JavascriptExecutor(parent), where WebDriver and JavascriptExecutor has no relation so we have to user casting "JavascriptExecutor js = (JavascriptExecutor) driver;"
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].setAttribute('value','Anil kumar')",inputbox); // this will send the value = Anil kumar in to the inputbox field 
+
+	}
+
+}
